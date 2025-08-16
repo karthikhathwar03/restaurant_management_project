c89a342c8e5f5562e2ca2701dfb84828abccd243
T L karthik hathwar <karthikhathwar123@gmail.com>
2025-08-16 17:38:11 +0530

    28434

--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
