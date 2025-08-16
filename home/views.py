67e75bc5fe8ace640695d5887af162aa5174167e
T L karthik hathwar <karthikhathwar123@gmail.com>
2025-08-16 17:48:05 +0530

    28434

--- home/views.py
+++ home/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Restaurant
 
 # Create your views here.
+def homepage_view(request):
+    try:
+        restaurant = Restaurant.objects.first()
+        restaurant_name = restaurant.name if restaurant else "Default Restaurant"
+    except Restaurant.DoesNotExist:
+        restaurant_name = 'Default Restaurant'
+    return render(request,'homepage.html',{'restaurant_name': restaurant_name})
